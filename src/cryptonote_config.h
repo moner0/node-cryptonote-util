45692f9643f956870b67fb88d450e7bd1f81f206
Mark Allen Evans <president@worldvaporexpo.com>
2018-02-23 02:28:01 -0500

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -6,10 +6,10 @@
 #define CURRENT_BLOCK_MINOR_VERSION                     0
 
 #define BLOCK_MAJOR_VERSION_1                           1
-#define BLOCK_MAJOR_VERSION_2                           3
+#define BLOCK_MAJOR_VERSION_2                           6
 
 #define COIN                                            ((uint64_t)1000000000000) // pow(10, 12)
-#define DEFAULT_FEE                                     ((uint64_t)400000000) // pow(4, 8)
+#define DEFAULT_FEE                                     ((uint64_t)10000000000) // pow(10, 10)
 
 
 #define CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW            60

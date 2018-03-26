ec0b1d4a4c64d027cce77c525c61e120533fe5e4
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-26 00:40:22 -0400

    staged

--- src/ringct/rctTypes.h
+++ src/ringct/rctTypes.h
@@ -47,10 +47,7 @@ extern "C" {
 }
 #include "crypto/crypto.h"
 
-#include "serialization/serialization.h"
-#include "serialization/debug_archive.h"
-#include "serialization/binary_archive.h"
-#include "serialization/json_archive.h"
+
 
 
 //Define this flag when debugging to get additional info on the console
